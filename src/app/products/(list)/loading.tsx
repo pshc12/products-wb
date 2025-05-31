cb22a4ff0096432e012e416cab6aadc2752cffcd
Sinehye Park <parksinehye@gmail.com>
2025-06-01 06:49:32 +0900

    add loading

--- src/app/products/(list)/loading.tsx
+++ src/app/products/(list)/loading.tsx
@@ -1,4 +1,11 @@
+import { Loader2Icon } from 'lucide-react';
+
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return 'loading...'; // todo
+  return (
+    <div className="h-[40vh] relative">
+      <div className="absolute inset-0 flex items-center justify-center">
+        <Loader2Icon className="animate-spin" />
+      </div>
+    </div>
+  );
 }
