e0a1115dadb936c6bb17a471402e3ff7b68a7d04
Sinehye Park <parksinehye@gmail.com>
2025-05-31 22:33:53 +0900

    add tanstack query

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,6 +17,7 @@ const eslintConfig = [
     'next/typescript',
     'prettier'
   ),
+  ...pluginQuery.configs['flat/recommended'],
 ];
 
 export default eslintConfig;
