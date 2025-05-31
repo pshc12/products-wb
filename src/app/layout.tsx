d7ea5786e35ad13e17a4e11187710da209b834f2
Sinehye Park <parksinehye@gmail.com>
2025-05-31 22:19:45 +0900

    add prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Weebur Project",
-  authors: [{name: '박신혜'}]
+  title: 'Weebur Project',
+  authors: [{ name: '박신혜' }],
 };
 
 export default function RootLayout({
