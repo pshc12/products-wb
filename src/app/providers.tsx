2048c191dcabe68b48f2269e182dbbed249e38a1
Sinehye Park <parksinehye@gmail.com>
2025-06-03 06:21:41 +0900

    remove unused react-query

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,6 @@
 import { ProductListStoreProvider } from '@/providers/product-list-store';
-import QueryProvider from '@/providers/query';
 import { ReactNode } from 'react';
 
 export default function Providers({ children }: { children: ReactNode }) {
-  return (
-    <QueryProvider>
-      <ProductListStoreProvider>{children}</ProductListStoreProvider>
-    </QueryProvider>
-  );
+  return <ProductListStoreProvider>{children}</ProductListStoreProvider>;
 }
